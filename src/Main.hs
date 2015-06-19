96d3fe9de5df332e35d75d279531caba9e79f8dc
Ecky Putrady <eckyputrady@gmail.com>
2015-06-19 09:25:47 +0700

    Re orgz

--- src/Main.hs
+++ src/Main.hs
@@ -8,186 +8,39 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
-import           Control.Applicative          ((<$>), (<*>))
-import           Control.Monad.State.Class    (MonadState)
-import           Control.Monad.Trans.Resource (runResourceT)
-import qualified Data.ByteString              as BS
-import           Data.Conduit
-import qualified Data.Conduit.Binary          as B
-import qualified Data.Conduit.List            as CL
-import qualified Data.Conduit.Text            as CT
-import qualified Data.Text                    as T
-import           Data.Time                    (UTCTime, diffUTCTime,
-                                               getCurrentTime)
-import           Data.Time                    (NominalDiffTime, TimeZone,
-                                               getCurrentTimeZone,
-                                               utcToLocalTime)
-import           Data.Time.Format             (defaultTimeLocale, formatTime)
-import           Options.Applicative
-import           System.Console.Haskeline
+import Control.Monad.Logger (MonadLogger, LoggingT(..), runStdoutLoggingT)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Control.Monad.Trans.Reader (ReaderT(..))
+import Data.Text (Text)
+import Database.Persist.MySQL (ConnectionPool, createMySQLPool, defaultConnectInfo, ConnectInfo(..))
 
-import           Database.Esqueleto
-import           Database.Persist.MySQL       (withMySQLPool, defaultConnectInfo, connectDatabase)
-import           Database.Persist.TH
+type AppM = ReaderT Config (LoggingT IO)
 
-import           Control.Monad.IO.Class       (MonadIO, liftIO)
-import           Control.Monad.Logger         (LoggingT, runStdoutLoggingT)
-import           Control.Monad.Logger         (MonadLogger)
+data Config = Config 
+  { getPool :: ConnectionPool }
 
---data Log = Log Tag Message UTCTime deriving (Show, Read)
---type Tag = String
---type Message = String
+-- defaultConfig :: (MonadIO m, MonadBaseControl a m, MonadLogger m) => m Config
+defaultConfig = do
+  pool <- createPool
+  return $ Config { getPool = pool
+                  }
 
---data Action = AddLog Tag Message
---            | List Int
---            deriving (Show, Read)
+-- createPool :: (MonadIO m, MonadBaseControl a m, MonadLogger m) => m ConnectionPool
+createPool = 
+  flip createMySQLPool 10 $ defaultConnectInfo  { connectUser = "root"
+                                                , connectPassword = "root"
+                                                , connectDatabase = "livelog"
+                                                } 
 
---main :: IO ()
---main = execParser opts >>= process
---  where opts = info (helper <*> commands) (fullDesc <> header "Log your activity")
---        commands = subparser ( logCommand <> listCommand )
---        logCommand = command "log" (info logOptions (progDesc "Log an activity"))
---        logOptions = AddLog <$> (argument str (metavar "TAG")) <*> (argument str (metavar "MESSAGE"))
---        listCommand = command "list" (info listOptions (progDesc "List your N last logged activities"))
---        listOptions = List <$> (argument auto (metavar "N"))
+run :: AppM a -> m Config -> IO a
+run app cfgM = runStdoutLoggingT $ do
+  cfg <- cfgM
+  return (runReaderT app cfg)
 
---process :: Action -> IO ()
---process (AddLog tag msg) = do
---  log <- (Log tag msg) <$> getCurrentTime
---  appendFile "db.txt" $ ((show log) <> "\n")
+main :: IO ()
+main = 
+  run app defaultConfig
 
---process (List n) = do
---  logs <- runResourceT $  B.sourceFile "db.txt" $$
---                          B.lines =$=
---                          CT.decode CT.utf8 =$=
---                          CL.map (\x -> (read $ T.unpack x) :: Log) =$
---                          CL.consume
---  curTime <- getCurrentTime
---  mapM_ (printPrettyLog) (withDuration curTime $ lastN n logs)
-
---withDuration :: UTCTime -> [Log] -> [(Log, NominalDiffTime)]
---withDuration endTime logs =
---  zip logs durations
---  where durations = zipWith diffUTCTime ((tail times) ++ [endTime]) times
---        times = map (\(Log _ _ time) -> time) logs
-
---printPrettyLog :: (Log, NominalDiffTime) -> IO ()
---printPrettyLog logAndDiff = do
---  timezone <- getCurrentTimeZone
---  putStrLn (prettyLog timezone logAndDiff)
-
---prettyLog :: TimeZone -> (Log, NominalDiffTime) -> String
---prettyLog timezone ((Log tag msg time), diff) =
---  prettyTime <> " - " <> duration <> "\n  tag: " <> tag <> "\n  " <> msg
---  where prettyTime = fmtTime (utcToLocalTime timezone time)
---        duration = (show $ durationSecs / 60) <> " minutes"
---        durationSecs = diff
---        fmtTime = formatTime defaultTimeLocale "%a %Y/%m/%e %H:%M:%S"
-
---lastN :: Int -> [a] -> [a]
---lastN n = reverse . take n . reverse
-
---- DB stuffs
-
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Tag
-  name  String
-  UniqueTagName name
-  deriving Show
-
-Log
-  message String
-  createdAt UTCTime
-  deriving Show
-
-TagLog
-  tagId TagId
-  logId LogId
-  UniqueTagLog tagId logId
-  deriving Show
-|]
-
---withDB :: MonadIO m => (ConnectionPool -> m ()) -> m ()
-withDB f = withMySQLPool connInfo 10 $ \pool -> do
-  flip runSqlPool pool $ runMigration migrateAll
-  f pool
-  where connInfo = defaultConnectInfo { connectDatabase = "livelog" }
-
--- data CreateLogData = CreateLogData { cldMsg :: String, cldTagName :: String }
--- --createLog :: CreateLogData -> ConnectionPool -> IO ()
--- createLog param pool = flip runSqlPool pool $ do
---   l <- insert $ Log (cldMsg param)
---   t <- insert $ Tag (cldTagName param)
---   insert $ TagLog t l
-
--- test f = withDB $ \pool -> do
---   return $ flip runSqlPersistMPool pool $ do
---     l <- insert $ Log "test"
---     t <- insert $ Tag "tag"
---     insert $ TagLog t l
---   return ()
---   f
-
-test f = runStdoutLoggingT . withDB $ \pool -> do
-  time <- liftIO getCurrentTime
-  flip runSqlPool pool $ f time
-
--- get last n logs
-getLogs n = 
-  select $ from $ \log -> do
-  orderBy [desc (log ^. LogCreatedAt)]
-  limit n
-  return $ log
-
--- get log
--- getLog id = get $ Key (PersistInt64 $ fromIntegral id)
-
--- create log
-createLog msg tagName time = do
-  logId <- insert $ Log msg time
-  tagLog logId tagName
-
--- edit log msg
-
--- get tags
-getTags n =
-  select $ from $ \tag -> do
-  orderBy [asc (tag ^. TagId)]
-  limit n
-  return $ tag
-
--- get tag
-getTag tagName = do
-  tags <- select $ from $ \tag -> do
-          where_ (tag ^. TagName ==. val tagName)
-          limit 1
-          return tag
-  return $ case tags of
-    []  -> Nothing
-    [x] -> Just x  
-
--- create tag
-createTag tagName = do
-  maybeTag <- getTag tagName
-  case maybeTag of
-    Nothing -> insert $ Tag tagName
-    Just v  -> return $ entityKey v
-
--- edit tag name
-editTag tagId tagName = do
-  update $ \tag -> do
-    set tag [TagName =. val tagName]
-    where_ (tag ^. TagId ==. val tagId)
-
--- add log tagging
-tagLog logId tagName = do
-  tagId <- createTag tagName
-  insert $ TagLog tagId logId
-
--- rem log tagging
-untagLog logId tagName = do
-  maybeTag <- getTag tagName
-  case maybeTag of
-    Nothing -> return ()
-    Just v  -> delete $ from $ \tagging -> do
-                where_ (tagging ^. TagLogTagId ==. val (entityKey v) &&. tagging ^. TagLogLogId ==. val logId)
+app :: AppM ()
+app = undefined
