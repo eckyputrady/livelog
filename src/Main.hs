026b9acd1f9871b58b8054257d17ab4e607f378b
Ecky Putrady <eckyputrady@gmail.com>
2015-06-22 07:55:13 +0700

    WIP

--- src/Main.hs
+++ src/Main.hs
@@ -1,74 +1,184 @@
-{-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad.Logger (MonadLogger, LoggingT(..), runStdoutLoggingT)
-import Control.Monad.IO.Class (MonadIO(..))
-import Control.Monad.Trans.Control (MonadBaseControl)
-import Control.Monad.Trans.Reader (ReaderT(..), asks)
-import Data.Text (Text)
-import Database.Esqueleto
-import Database.Persist.TH
-import Database.Persist.MySQL (ConnectionPool, createMySQLPool, defaultConnectInfo, ConnectInfo(..))
-import Database.Persist.Sql (runSqlPool)
+module Main where
 
-type AppM = ReaderT Config (LoggingT IO)
+import Control.Applicative
+import Control.Monad.Trans (MonadTrans)
+import Control.Monad.Reader (MonadIO, MonadReader, ReaderT, asks, lift, runReaderT)
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.IO.Class (liftIO)
+import Data.Default.Class (def)
+import Data.Text.Lazy (Text, pack)
+import Data.ByteString (ByteString)
+import Prelude
+import Web.Scotty.Trans
+import qualified Database.Persist as DB
+import Database.Persist.MySQL (ConnectionPool, createMySQLPool, defaultConnectInfo, connectDatabase, connectPassword)
+import Database.Persist.Sql (runSqlPool, fromSqlKey, toSqlKey)
+import Network.HTTP.Types (status404, status201)
+import Network.Wai (Middleware(..), vault)
+import Data.Text.Lazy.Encoding (decodeUtf8)
 
-data Config = Config 
-  { getPool :: ConnectionPool }
+import qualified Data.Vault.Lazy as Vault
+import Web.ClientSession (getDefaultKey)
+import Network.Wai.Session (withSession, Session, SessionStore)
+import Network.Wai.Session.ClientSession (clientsessionStore)
+import Data.Serialize (encode, decode)
 
-defaultConfig :: IO Config
-defaultConfig = do
-  pool <- createPool
-  return $ Config { getPool = pool
-                  }
-createPool :: IO ConnectionPool
-createPool = runStdoutLoggingT $
-  flip createMySQLPool 10 $ defaultConnectInfo  { connectUser = "root"
-                                                , connectPassword = "root"
-                                                , connectDatabase = "livelog"
-                                                } 
+import Model
 
-run :: AppM a -> Config -> IO a
-run app cfg = 
-  runStdoutLoggingT $ runReaderT app cfg
+data Config = Config
+  { pool :: ConnectionPool
+  , vaultKey :: Vault.Key (Session IO ByteString ByteString)
+  }
+
+newtype ConfigM a = ConfigM
+  { runConfigM :: ReaderT Config IO a
+  } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
+
+type AppM = ScottyT Text ConfigM
+type ActM = ActionT Text ConfigM
+
+application :: [Middleware] -> AppM ()
+application mws = do
+  -- middlewares
+  mapM_ middleware mws
+
+  -- routes
+  sessionRoutes
+  userRoutes
+  logRoutes
+  tagRoutes
+  notFound notFoundA
+
+notFoundA = do
+  status status404
 
 main :: IO ()
-main = do
-  cfg <- defaultConfig
-  run app cfg
+main = do 
+  c   <- getConfig
+  mws <- sequence [sessionMW c]
+  runSqlPool migrateModels (pool c)
+  scottyOptsT def (runIO c) $ application mws
+  where
+    runIO :: Config -> ConfigM a -> IO a
+    runIO c m = runReaderT (runConfigM m) c
 
-app :: AppM a
-app = undefined
+    sessionMW :: Config -> IO Middleware
+    sessionMW c = do
+      sstore <- clientsessionStore <$> getDefaultKey
+      return $ withSession sstore "session" def (vaultKey c)
 
--- runDB :: AppM a
-runDB query = do
-  pool <- asks getPool
-  runSqlPool query pool
+getConfig = do
+  p   <- runStdoutLoggingT $ createMySQLPool connInfo 10
+  vk  <- Vault.newKey
+  return $ Config { pool = p, vaultKey = vk }
+  where connInfo = defaultConnectInfo { connectDatabase = "livelog", connectPassword = "" }
+
+withDB q = do
+  p <- lift $ asks pool
+  liftIO $ runSqlPool q p
+
+---
+
+getSession = do
+  vk <- lift $ asks vaultKey
+  req <- request
+  return $ Vault.lookup vk (vault req)
+
+requireUser :: ActM (Maybe UserId)
+requireUser = do
+  Just (sessionLookup, _) <- getSession
+  maybeUId <- liftIO $ sessionLookup "u"
+  case maybeUId of 
+    Nothing -> raise "Unknown"
+    Just v -> case decode v of
+      Left _ -> raise "Format error"
+      Right decoded -> do
+        let k = toSqlKey decoded
+        mUser <- withDB $ DB.get (k :: UserId)
+        liftIO $ putStrLn $ "you are " ++ (show mUser)
+        case mUser of
+          Nothing -> raise "Unknown user"
+          Just user  -> do
+            return $ Just k
+
+sessionRoutes :: AppM ()
+sessionRoutes = do
+  post    "/sessions"   _save
+  delete  "/sessions"   _delete
+  where
+    _save = do
+      d <- jsonData
+      vk <- lift $ asks vaultKey
+      req <- request
+      let Just (_, sessionInsert) = Vault.lookup vk (vault req)
+      user <- withDB $ DB.getByValue (d :: User)
+      case user of
+        Nothing -> 
+          notFoundA
+        Just v -> do
+          liftIO $ sessionInsert "u" $ encode . fromSqlKey . DB.entityKey $ v 
+          status status201
+
+    _delete = do
+      Just (_, sessionInsert) <- getSession
+      liftIO $ sessionInsert "u" ""
 
 
--- Model
+userRoutes :: AppM ()
+userRoutes = do
+  post "/users"   _save
+  where
+    _save = do
+      b <- jsonData
+      l <- withDB $ createUser (b :: User)
+      status status201
+      json l
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Tag
-  name  String
-  UniqueTagName name
-  deriving Show
+logRoutes :: AppM ()
+logRoutes = do
+  get "/logs"     $ requireUser >>= _query
+  post "/logs"    _save
+  get "/logs/:id" _get
+  where
+    _query user = do
+      logs <- withDB (getLogs 10)
+      json logs
 
-Log
-  message String
-  createdAt UTCTime
-  deriving Show
+    _get = do
+      i   <- param "id"
+      log <- withDB $ getLog . fromIntegral $ (i :: Int)
+      case log of
+        Nothing -> notFoundA
+        Just v  -> json v
 
-TagLog
-  tagId TagId
-  logId LogId
-  UniqueTagLog tagId logId
-  deriving Show
-|]
+    _save = do
+      b <- jsonData
+      l <- withDB $ createLog (b :: Log)
+      status status201
+      json l
+
+tagRoutes :: AppM ()
+tagRoutes = do
+  get   "/tags"     _query
+  post  "/tags"     _save
+  get   "/tags/:id" _get
+  where
+    _query = do
+      tags <- withDB (getTags 10)
+      json tags
+
+    _get = do
+      i   <- param "id"
+      log <- withDB . DB.get . toSqlKey . fromIntegral $ (i :: Int)
+      json (log :: Maybe Log)
+
+    _save = do
+      b <- jsonData
+      l <- withDB $ createLog (b :: Tag)
+      status status201
+      json l
+
+
---
+++ src/Model.hs
@@ -0,0 +1,153 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE FlexibleInstances               #-}
+
+module Model where
+
+import Control.Applicative ((<$>))
+import Data.Monoid ((<>))
+import Data.Time (UTCTime)
+import Control.Monad.IO.Class (MonadIO(..))
+import Database.Esqueleto
+import Database.Persist.TH
+import Database.Persist.MySQL (ConnectionPool, createMySQLPool, defaultConnectInfo, ConnectInfo(..))
+import Database.Persist.Sql (runSqlPool)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+User json
+  name String
+  pass String
+  UniqueUnamePass name pass
+  UniqueUname name
+  deriving Show
+
+Tag json
+  name  String
+  UniqueTagName name
+  deriving Show
+
+Log json
+  message String
+  createdAt UTCTime
+  deriving Show
+
+TagLog json
+  tagId TagId
+  logId LogId
+  UniqueTagLog tagId logId
+  deriving Show
+|]
+
+-- running the migration
+migrateModels :: (MonadIO m) => SqlPersistT m ()
+migrateModels = runMigration migrateAll
+
+-- /// Users
+
+createUser u = do
+  i <- insert (u :: User)
+  get i
+
+getUser u = do
+  x <-  select $ from $ \user -> do
+        where_ (user ^. UserName ==. u ^. UserName)
+        limit 1
+        return log
+  return $ case x of 
+    []  -> Nothing
+    [v] -> Just v
+
+-- createSession = 
+
+-- /// LOGS
+
+-- get last n logs
+getLogs n = 
+  select $ from $ \log -> do
+  orderBy [desc (log ^. LogCreatedAt)]
+  limit n
+  return $ log
+
+-- get log
+getLog id = do
+  x <-  select $ from $ \log -> do
+        where_ (log ^. LogId ==. val (toSqlKey id))
+        limit 1
+        return log
+  return $ case x of 
+    []  -> Nothing
+    [v] -> Just v
+
+-- createLog
+createLog log = do
+  i <- insert log
+  get i
+
+-- create log
+-- createLogWithTag msg tagName time = do
+--   maybeL <- createLog (Log msg time)
+--   case maybeL of
+--     Just l  -> tagLog (entityKey l) tagName
+--     Nothing -> Nothing
+
+-- edit log msg
+editLog logId msg time =
+  update $ \log -> do
+  set log (msgUpdate msg <> timeUpdate time)
+  where_ (log ^. LogId ==. val (toSqlKey logId))
+  where
+    msgUpdate (Just msg) = [LogMessage =. val msg]
+    msgUpdate _ = []
+    timeUpdate (Just time) = [LogCreatedAt =. val time]
+    timeUpdate _ = []
+
+-- //// TAGS
+
+-- get tags
+getTags n =
+  select $ from $ \tag -> do
+  orderBy [asc (tag ^. TagId)]
+  limit n
+  return $ tag
+
+-- get tag
+getTag key = do 
+  let k = toSqlKey key :: TagId
+  v <- get k
+  return $ Entity k <$> v
+getTagByName tagName = getBy $ UniqueTagName tagName 
+
+-- create tag
+createTag tagName = do
+  maybeTag <- getTagByName tagName
+  case maybeTag of
+    Nothing -> insert $ Tag tagName
+    Just v  -> return $ entityKey v
+
+-- edit tag name
+editTag tagId tagName =
+  update $ \tag -> do
+  set tag [TagName =. val tagName]
+  where_ (tag ^. TagId ==. val tagId)
+
+-- //// TagLog
+
+-- add log tagging
+tagLog logId tagName = do
+  tagId <- createTag tagName
+  insert $ TagLog tagId logId
+
+-- rem log tagging
+untagLog logId tagName = do
+  maybeTag <- getTagByName tagName
+  case maybeTag of
+    Nothing -> return ()
+    Just v  -> delete $ from $ \tagging -> do
+                where_ (tagging ^. TagLogTagId ==. val (entityKey v) &&. tagging ^. TagLogLogId ==. val logId)
