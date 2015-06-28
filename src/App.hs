3b8e45d4c08692ad4da180d961ec61e0e841422c
Ecky Putrady <eckyputrady@gmail.com>
2015-06-28 09:56:25 +0700

    Remove unneeded code + separate main from app

---
+++ src/App.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module App where
+
+import Control.Applicative
+import Control.Monad.Trans (MonadTrans)
+import Control.Monad.Reader (MonadIO, MonadReader, ReaderT, asks, lift, runReaderT)
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.IO.Class (liftIO)
+import Data.Default.Class (def)
+import Data.Text.Lazy (Text, pack)
+import Data.ByteString (ByteString)
+import Prelude
+import Web.Scotty.Trans
+import qualified Database.Persist as DB
+import Database.Persist.MySQL (ConnectionPool, createMySQLPool, defaultConnectInfo, connectDatabase, connectPassword)
+import Database.Persist.Sql (runSqlPool, fromSqlKey, toSqlKey)
+import Network.HTTP.Types (status404, status201)
+import Network.Wai (Middleware(..), vault)
+import Data.Text.Lazy.Encoding (decodeUtf8)
+
+import qualified Data.Vault.Lazy as Vault
+import Web.ClientSession (getDefaultKey)
+import Network.Wai.Session (withSession, Session, SessionStore)
+import Network.Wai.Session.ClientSession (clientsessionStore)
+import Data.Serialize (encode, decode)
+
+import Model
+import Types
+import Controllers.Common
+import qualified Controllers.Logs as CLogs
+import qualified Controllers.Tags as CTags
+import qualified Controllers.LogTag as CLogTag
+
+application :: [Middleware] -> AppM ()
+application mws = do
+  -- middlewares
+  mapM_ middleware mws
+
+  -- routes
+  sessionRoutes
+  userRoutes
+  CLogs.routes
+  CTags.routes
+  CLogTag.routes
+  notFound notFoundA
+
+runApp :: Config -> IO ()
+runApp c = do
+  runSqlPool migrateModels (pool c)
+  mws <- sequence [sessionMW c]
+  scottyOptsT def (runIO c) $ application mws
+  where
+    runIO :: Config -> ConfigM a -> IO a
+    runIO c m = runReaderT (runConfigM m) c
+
+    sessionMW :: Config -> IO Middleware
+    sessionMW c = do
+      sstore <- clientsessionStore <$> getDefaultKey
+      return $ withSession sstore "session" def (vaultKey c)
+
+--
+
+sessionRoutes :: AppM ()
+sessionRoutes = do
+  post    "/sessions"   _save
+  delete  "/sessions"   _delete
+  where
+    _save = do
+      d <- jsonData
+      user <- withDB $ DB.getByValue (d :: User)
+      case user of
+        Nothing -> 
+          notFoundA
+        Just v -> do
+          (_, sessionInsert) <- getSession
+          liftIO $ sessionInsert "u" $ encode . fromSqlKey . DB.entityKey $ v 
+          status status201
+
+    _delete = do
+      (_, sessionInsert) <- getSession
+      liftIO $ sessionInsert "u" ""
+
+
+userRoutes :: AppM ()
+userRoutes = do
+  post "/users"   _save
+  where
+    _save = do
+      b <- jsonData
+      l <- withDB $ DB.insert (b :: User)
+      status status201
+      json l
+
--- src/Model.hs
+++ src/Model.hs
@@ -19,7 +19,6 @@ import Data.Time (UTCTime)
 import Control.Monad.IO.Class (MonadIO(..))
 import Database.Esqueleto
 import Database.Persist.TH
-import Database.Persist.MySQL (ConnectionPool, createMySQLPool, defaultConnectInfo, ConnectInfo(..))
 import Database.Persist.Sql (runSqlPool)
 import GHC.Int (Int64)
 
@@ -50,12 +49,36 @@ TagLog json
   deriving Show
 |]
 
--- running the migration
+-- Migration replated
+
 migrateModels :: (MonadIO m) => SqlPersistT m ()
 migrateModels = runMigration migrateAll 
 
+-- Common func
+
 toKey x = toSqlKey . fromIntegral $ x
 getById x = get . toKey $ x
+
+getFor getterF itemId =
+  select $ from $ \item -> do
+  where_ (item ^. getterF ==. val itemId)
+  return item
+
+getUserItem userF userId itemF itemId =
+  listToMaybe <$> results
+  where results = select $ from $ \item -> do
+                  where_ (item ^. itemF ==. val itemId &&. item ^. userF ==. val userId)
+                  limit 1
+                  return item
+
+delWithCond xs = 
+  delete $ from $ \item -> do
+  where_ (foldr (&&.) (val True) $ map (parseCond item) xs)
+  where 
+    parseCond item (getterF, getterVal) =
+      item ^. getterF ==. val getterVal
+
+-- LOGS
 
 data LogFilterParam = LogFilterParam
   { lfpMessage :: Maybe String
@@ -90,6 +113,8 @@ logQuery filterParam sortParam =
     parseDir Asc = asc
     parseDir Desc = desc
 
+-- LOG TAGS
+
 queryLogTags userId mLogId mTagId =
   select $ from $ \(log `InnerJoin` taglog `InnerJoin` tag) -> do
   on (tag ^. TagId ==. taglog ^. TagLogTagId)
@@ -105,142 +130,3 @@ queryLogTags userId mLogId mTagId =
 delLogTags logId tagId =
   delete $ from $ \taglog -> do
   where_ (taglog ^. TagLogLogId ==. val logId &&. taglog ^. TagLogTagId ==. val tagId)
-
-
--- LOGS
-
--- getLogsFor :: Entity User -> SqlPersistT m [Entity Log]
--- getLogsFor userId =
---   select $ from $ \log -> do
---   where_ (log ^. LogUserId ==. val userId)
---   return log
-
-getFor getterF itemId =
-  select $ from $ \item -> do
-  where_ (item ^. getterF ==. val itemId)
-  return item
-
-getWithCond xs = 
-  select $ from $ \item -> do
-  where_ (foldr (&&.) (val True) $ map (parseCond item) xs)
-  return item
-  where 
-    parseCond item (getterF, getterVal) =
-      item ^. getterF ==. val getterVal
-
-getUserItem userF userId itemF itemId =
-  listToMaybe <$> results
-  where results = select $ from $ \item -> do
-                  where_ (item ^. itemF ==. val itemId &&. item ^. userF ==. val userId)
-                  limit 1
-                  return item
-
-delWithCond xs = 
-  delete $ from $ \item -> do
-  where_ (foldr (&&.) (val True) $ map (parseCond item) xs)
-  where 
-    parseCond item (getterF, getterVal) =
-      item ^. getterF ==. val getterVal
-
--- /// Users
-
--- createUser u = do
---   i <- insert (u :: User)
---   get i
-
--- getUser u = do
---   x <-  select $ from $ \user -> do
---         where_ (user ^. UserName ==. u ^. UserName)
---         limit 1
---         return log
---   return $ case x of 
---     []  -> Nothing
---     [v] -> Just v
-
--- -- createSession = 
-
--- -- /// LOGS
-
--- -- get last n logs
--- getLogs n = 
---   select $ from $ \log -> do
---   orderBy [desc (log ^. LogCreatedAt)]
---   limit n
---   return $ log
-
--- -- get log
--- getLog id = do
---   x <-  select $ from $ \log -> do
---         where_ (log ^. LogId ==. val (toSqlKey id))
---         limit 1
---         return log
---   return $ case x of 
---     []  -> Nothing
---     [v] -> Just v
-
--- -- createLog
--- createLog log = do
---   i <- insert log
---   get i
-
--- -- create log
--- -- createLogWithTag msg tagName time = do
--- --   maybeL <- createLog (Log msg time)
--- --   case maybeL of
--- --     Just l  -> tagLog (entityKey l) tagName
--- --     Nothing -> Nothing
-
--- -- edit log msg
--- editLog logId msg time =
---   update $ \log -> do
---   set log (msgUpdate msg <> timeUpdate time)
---   where_ (log ^. LogId ==. val (toSqlKey logId))
---   where
---     msgUpdate (Just msg) = [LogMessage =. val msg]
---     msgUpdate _ = []
---     timeUpdate (Just time) = [LogCreatedAt =. val time]
---     timeUpdate _ = []
-
--- -- //// TAGS
-
--- -- get tags
--- getTags n =
---   select $ from $ \tag -> do
---   orderBy [asc (tag ^. TagId)]
---   limit n
---   return $ tag
-
--- -- get tag
--- getTag key = do 
---   let k = toSqlKey key :: TagId
---   v <- get k
---   return $ Entity k <$> v
--- getTagByName tagName = getBy $ UniqueTagUserIdName tagName 
-
--- -- create tag
--- createTag tagName = do
---   maybeTag <- getTagByName tagName
---   case maybeTag of
---     Nothing -> insert $ Tag tagName
---     Just v  -> return $ entityKey v
-
--- -- edit tag name
--- editTag tagId tagName =
---   update $ \tag -> do
---   set tag [TagName =. val tagName]
---   where_ (tag ^. TagId ==. val tagId)
-
--- -- //// TagLog
-
--- -- add log tagging
--- tagLog logId tagName = do
---   tagId <- createTag tagName
---   insert $ TagLog tagId logId
-
--- -- rem log tagging
--- untagLog logId tagName = do
---   maybeTag <- getTagByName tagName
---   case maybeTag of
---     Nothing -> return ()
---     Just v  -> delete $ from $ \tagging -> do
---                 where_ (tagging ^. TagLogTagId ==. val (entityKey v) &&. tagging ^. TagLogLogId ==. val logId)
