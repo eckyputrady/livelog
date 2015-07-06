2e3f22a98660b9bf8b83a79d77097f7ce5eaa507
Ecky Putrady <eckyputrady@gmail.com>
2015-07-07 02:27:37 +0700

    Update build script to include packaging

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ cabal update
 cabal install --only-dependencies --enable-tests
 cabal test
 cabal build
+
+echo "Packaging application ..."
+mkdir -p dist/livelog
+cp dist/build/livelog-exe/livelog-exe dist/livelog/livelog-exe
+cp config.json dist/livelog/config.json
+cp client_session_key.aes dist/livelog/client_session_key.aes
+echo "Packaging application DONE"
