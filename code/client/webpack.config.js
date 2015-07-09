b7fd46910960d5c36325be58233bc19466f46133
Ecky Putrady <eckyputrady@gmail.com>
2015-07-09 22:22:22 +0700

    Setup infrastructure & skeleton for client.

---
+++ code/client/webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// import Webpack plugins
+var cleanPlugin = require('clean-webpack-plugin');
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
+// define Webpack configuration object to be exported
+var config = {
+    context: __dirname + '/app',
+    entry: './index.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js'
+    },
+    resolve: {
+      alias: {
+        'npm': __dirname + '/node_modules'
+      }
+    },    
+    module: {
+      loaders: [
+        { 
+          test: /\.css$/, 
+          loader: 'style!css'
+        },
+        { 
+          test: /\.(woff|woff2)$/,   
+          loader: 'url?limit=10000&mimetype=application/font-woff' 
+        },
+        { 
+          test: /\.(eot|svg|ttf)$/,    
+          loader: 'file' 
+        },
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel?stage=4!jshint'
+        }
+      ]
+    },
+    plugins: [
+      new cleanPlugin(['dist']),
+      new htmlWebpackPlugin()
+    ],
+    devServer: {
+      contentBase: __dirname + '/dist',
+      colors: true,
+      host: '0.0.0.0'
+    }
+};
+
+module.exports = config;
